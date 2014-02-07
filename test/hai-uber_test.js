375613a6eee47fc2564597ecd6f2bee011808cae
Todd Wolfson <todd@twolfson.com>
2014-02-07 11:26:04 -0800

    Added lib and tests

--- test/hai-uber_test.js
+++ test/hai-uber_test.js
@@ -1,11 +1,27 @@
-var hai_uber = require('../');
+var assert = require('assert');
+var request = require('request');
+var Server = require('../');
 
+// TODO: Show off perceptual diffs if we have time =)
 describe('hai-uber', function () {
   before(function () {
-
+    this.app = new Server(9002);
+  });
+  after(function (done) {
+    this.app.close(done);
   });
 
-  it('', function () {
+  describe('replying to a request', function () {
+    before(function (done) {
+      var that = this;
+      request('http://localhost:9002/', function (err, res, body) {
+        that.body = body;
+        done(err);
+      });
+    });
 
+    it('returns "Hello World!"', function () {
+      assert.strictEqual('Hello World!\n', this.body);
+    });
   });
 });
